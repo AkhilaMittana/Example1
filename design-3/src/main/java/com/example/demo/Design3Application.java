424c9d976b76a10d5f30d30b34d24bdd6c92ca6c
Akhila Mittana <Akhila Mittana@AkhilaMittana>
2021-01-21 22:28:14 +0530

    First commit.

---
+++ design-3/src/main/java/com/example/demo/Design3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Design3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Design3Application.class, args);
+	}
+
+}
